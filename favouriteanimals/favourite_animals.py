df500ea661a63125f3944d2d03b76b748258a7fa
Borbala Dobo <dobori@gmail.com>
2017-10-06 10:15:18 +0200

    feat(fav_animals): gitignore ready, basic functions ar under construction

--- favouriteanimals/favourite_animals.py
+++ favouriteanimals/favourite_animals.py
@@ -1,3 +1,5 @@
+import sys
+
 # The program's aim is to collect your favourite animals and store them in a text file.
 # There is a given text file called '''favourites.txt''', it will contain the animals
 # If ran from the command line without arguments
@@ -7,3 +9,22 @@
 # One animal should be stored only at once
 # Each animal should be written in separate lines
 # The program should only save animals, no need to print them
+
+
+
+class FavAnimal(object):
+    
+    index = ""
+    def __init__(self):
+        if len(sys.argv) == 1:
+            print("Usage: favoureite_animals [animal] [animal]")
+        if len(sys.argv) > 1:
+
+            self.index = str(sys.argv[2])
+            return self.add_todo(self.index)
+
+    def add_animal(self, index):
+        animal_input = self.index
+        self.textfile = open("favourites.txt", "a")
+        self.textfile.write("0 " + animal_input + "\n")
+        self.textfile.close()
